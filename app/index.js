8efe22ac79ef4003c866977763e292260298070c
victor-akinboro <victorakinboro2003@gmail.com>
2023-04-25 11:23:20 +0100

    added assets

---
+++ app/index.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import {COLORS, icons, images, SIZES} from '../constants'
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
+import { Stack, useRouter } from 'expo-router'
+
+export default function Home() {
+  const router = useRouter();
+  
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <Stack.Screen 
+        options={{
+          headerStyle: {backgroundColor: COLORS.lightWhite},
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimensions="100%" />
+          ),
+          headerTitle: ""
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
